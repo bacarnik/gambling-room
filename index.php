8fba42713d3068933cfe20676ee563b856eee798
bacarnik <bacarnik338@gmail.com>
2026-05-07 19:48:16 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,34 +1,48 @@
 <?php
     session_start();
+    $users = [];
+    if(isset($_POST['submit'])){
+        $users = [['username1' => $_POST['username1']], ['username2' => $_POST['username2']], ['username3' => $_POST['username3']]];
+    }  
+
+    $_SESSION['users'] = $users;
 ?>
 
 <!DOCTYPE html>
 <html>
+
 <head>
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/style_index.css">
+    <title>Gambling Room</title>
 </head>
+
 <body>
     <div id="container">
         <div id="input-container">
-            <form  action="process.php" method="POST">
-            <div class="user-group">
-                <label>User 1:</label>
-                <input type="text" name="usernames[]" placeholder="Enter name" required>
-            </div>
-            
-            <div class="user-group">
-                <label>User 2:</label>
-                <input type="text" name="usernames[]" placeholder="Enter name" required>
-            </div>
-            
-            <div class="user-group">
-                <label>User 3:</label>
-                <input type="text" name="usernames[]" placeholder="Enter name" required>
-            </div>
+            <form action="php/main.php" method="POST">
 
-            <button type="submit">Play</button>
-        </form>
+                <h1>Enter usernames</h1>
+                <div id="users-container">
+                    <div class="user-group">
+                        <label>USER 1:</label>
+                        <input type="text" name="username1" placeholder="Enter name" required>
+                    </div>
+
+                    <div class="user-group">
+                        <label>USER 2:</label>
+                        <input type="text" name="username2" placeholder="Enter name" required>
+                    </div>
+
+                    <div class="user-group">
+                        <label>USER 3:</label>
+                        <input type="text" name="username3" placeholder="Enter name" required>
+                    </div>
+                </div>
+
+                <button type="submit" id="play-button" >Play</button>
+            </form>
         </div>
     </div>
 </body>
+
 </html>
---
+++ php/main.php
@@ -0,0 +1,28 @@
+<?php
+    session_start();
+    
+?>
+
+<!DOCTYPE html>
+<html>
+
+<head>
+    <link rel="stylesheet" href="../css/style_main.css">
+    <title>Gambling Room</title>
+</head>
+
+<body>
+    <div id="container">
+        <h1>Gambling Room</h1>
+        <div id="game-container">
+            <div id="users-container">
+                <div class="user-card">abc</div>
+                <div class="user-card">abv</div>
+                <div class="user-card">def</div>
+            </div>
+            <hr>
+        </div>
+    </div>
+</body>
+
+</html>
